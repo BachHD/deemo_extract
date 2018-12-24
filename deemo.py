c36b6d50f25d480bf4c6027d38d7f126e03d74a9
Bach Ha <haducbach@gmail.com>
2018-12-24 16:28:33 +0100

    use fsb5

--- deemo.py
+++ deemo.py
@@ -1,8 +1,10 @@
 import os
 import glob
+import fsb5
 
 def is_valid(char):
     try:
+        #Lazy ASCII checking
         char_dec = ord(char)
         if (48 <= char_dec <= 57 or
             65 <= char_dec <= 90 or
@@ -14,17 +16,11 @@ def is_valid(char):
     except:
         pass
     return False
-
-data_dir = "Data"
-file_list = glob.glob(os.path.join(data_dir, "*"))
-
-for file_name in file_list:
-    if ((".resource" in file_name) or
-            ("sharedasset" in file_name)):
-        continue
-
+	
+def get_real_name(file_name):
     with open(file_name, "rb") as fp:
-        fp.seek(0x1004)
+        # Some file start at 0x1004 instead of 0x1005
+        fp.seek(0x1004)		
         if (is_valid(fp.read(1))):
             start_pos = 0x1004
         else:
@@ -36,12 +32,54 @@ for file_name in file_list:
             offset += 1
 
         if (offset < 1):
-            continue
+            return None
 
         fp.seek(start_pos)
         real_name = fp.read(offset)
-        new_path = os.path.join(data_dir, real_name.decode("ascii").strip())
-        while (os.path.isfile(new_path)):
-            new_path += "d"
-        print("{} > {}".format(file_name, new_path))
-        os.rename(file_name, new_path)
+        return real_name
+
+
+
+source_dir = "Data"
+destination_dir = "Out"
+
+if not os.path.exists(destination_dir):
+    os.makedirs(destination_dir)
+
+#Songs are compressed into FSB5 files and named some_string.resource
+#Song name is in file with the same as the file above, but without .resource
+res_file_list = glob.glob(os.path.join(source_dir, "*.resource"))
+for res_file in res_file_list:
+    if ("sharedasset" in res_file):
+        continue
+
+    #Get real name
+    name_file = res_file.split(".")[0]
+    real_name = get_real_name(name_file)
+    if (real_name is None):
+        continue
+	
+    #Extract FSB5
+    with open(res_file, 'rb') as fp:
+        fsb = fsb5.FSB5(fp.read())
+        
+    audio_data = fsb.samples[0]
+    
+    #Check extension
+    ext = fsb.get_sample_extension()
+    if (ext is not "mp3"):
+        continue
+    
+    #Build audio save path
+    audio_path = os.path.join(destination_dir, real_name.decode("ascii").strip())
+    while (os.path.isfile(audio_path)):
+        audio_path += "d"
+        
+    audio_path = audio_path + "." + ext
+    
+    print("{} > {}".format(res_file, audio_path))
+    
+    #Write audio file
+    with open(audio_path, 'wb') as fp:
+        rebuilt_sample = fsb.rebuild_sample(audio_data)
+        fp.write(rebuilt_sample)    
